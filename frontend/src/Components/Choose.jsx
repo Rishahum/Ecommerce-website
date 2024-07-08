4ea151f360524e2b1c8009dd84f410d95aa35662
Risha Sinha <98969301+Rishahum@users.noreply.github.com>
2024-07-08 09:36:15 +0530

    Delete frontend directory

--- frontend/src/Components/Choose.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Choose = () => {
-  return (
-    <div>Choose</div>
-  )
-}
--- frontend/src/Components/Review.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Typography } from '@mui/material'
-export const Review = () => {
-  return (
-    <div>Review
-        <Typography>Hello</Typography>
-    </div>
-  )
-}
