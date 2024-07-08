6cce618d2bcb8c545c0c48cb6f54ba9849b111c6
Risha Sinha <98969301+Rishahum@users.noreply.github.com>
2024-07-08 18:17:22 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -18,3 +18,28 @@ exports.registerUser = catchAsyncErrors(async(req, res)=>{
         token
     })
 })
+exports.loginUser = catchAsyncErrors(async(req,res)=>{
+    const {email, password} = req.body;
+
+    //checking if user or password is enter
+    if(!email || !password){
+        return next(new ErrorHandler("Please enter your email and password", 404))
+    }
+   const user = await User.findOne({email}).select("+password")
+    
+   if(!user ){
+    return next(new ErrorHandler("Please Enter valid Email and password",404))
+   }
+    //check password 
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if( !isPasswordMatched){
+        return next(new ErrorHandler("Please Enter valid Email and password",404))
+       }
+       const token = user.getJWTToken()
+       res.status(200).json({
+           success:true,
+           token
+       })
+
+})
