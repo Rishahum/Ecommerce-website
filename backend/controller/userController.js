6b3206d6f0d7bd296cd9220deadb7ac728c895cb
Risha Sinha <98969301+Rishahum@users.noreply.github.com>
2024-07-12 10:27:11 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -43,3 +43,14 @@ exports.loginUser = catchAsyncErrors(async(req,res)=>{
        })
 
 })
+exports.logout = catchAsyncErrors(async(req, res, next)=>{
+    res.cookie= ("token", null,{
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "logged out"
+    })
+})
